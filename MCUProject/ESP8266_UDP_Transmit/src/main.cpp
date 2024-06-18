e27bea1d594e0b2a54291484bf74f8cee3e3b8b9
tomixrm <chocolate0785lego@icloud.com>
2024-06-18 16:48:37 +0900

    hostnameで送信先のIPを取得できるようにした

--- MCUProject/ESP8266_UDP_Transmit/src/main.cpp
+++ MCUProject/ESP8266_UDP_Transmit/src/main.cpp
@@ -1,18 +1,21 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
-#include <WiFiUDP.h>
+#include <WiFiUdp.h>
 
 #define SSID "TP-Link_AIOL"
 #define PASSWORD "AIOL2018"
-
 #define UDP_PORT 12345
 
+#define LED_WiFi_CONNECTED D1
+
 WiFiUDP udp;
-IPAddress serverIP(192, 168, 0, 135); // PCのIPアドレス
+// IPAddress serverIP(192, 168, 0, 153); // コメントアウト: PCのIPアドレス
+const char* serverHostname = "tomixrm-2.local"; // 送信先のホスト名
 
 void setup() {
     Serial.begin(115200);
     pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(LED_WiFi_CONNECTED, OUTPUT);
     delay(10);
 
     WiFi.mode(WIFI_STA);
@@ -34,23 +37,49 @@ void setup() {
 
     udp.begin(UDP_PORT);
     delay(1000);
-
+    digitalWrite(LED_WiFi_CONNECTED, HIGH);
     digitalWrite(LED_BUILTIN, HIGH);
     delay(100);
     digitalWrite(LED_BUILTIN, LOW);
     delay(100);
 }
 
+int wifilevel(bool getRaw = false) {
+    if (WiFi.status() != WL_CONNECTED)
+        return 0;
+    else {
+        int rssi = WiFi.RSSI();
+        if (getRaw)
+            return rssi;
+        int siglevel;
+        if (rssi <= -96)
+            siglevel = 1;
+        else if (rssi <= -85)
+            siglevel = 2;
+        else if (rssi <= -75)
+            siglevel = 3;
+        else
+            siglevel = 4;
+        return siglevel;
+    }
+}
+
 void loop() {
     static int i = 0;
     i++;
-    Serial.println("Hello, world!");
+    Serial.printf("Hello, world! %d\n", wifilevel(true));
     delay(10);
 
-    udp.beginPacket(serverIP, UDP_PORT); // 送信先のIPアドレスとポート番号を指定
+    IPAddress serverIP;
+    if (WiFi.hostByName(serverHostname, serverIP)) {
+        udp.beginPacket(serverIP, UDP_PORT); // 送信先のIPアドレスとポート番号を指定
 
-    char buf[128];
-    sprintf(buf, "Hello, world! %d", i);
-    udp.write(buf); // 送信するデータ
-    udp.endPacket();
+        char buf[128];
+        sprintf(buf, "Hello, world! %d", i);
+        udp.write(buf); // 送信するデータ
+        udp.endPacket();
+    } else {
+        Serial.println("Hostname resolution failed");
+    }
+    delay(10);
 }
